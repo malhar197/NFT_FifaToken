e72c2d5f2278a03bab2deee57b6b10c4cf545474
Malhar Dave <malhar197@Malhars-MacBook-Air.local>
2020-06-06 17:04:09 -0400

    boilerplate code and flattened ERC721.sol

---
+++ contracts/FifaToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.6.0;
+
+import "./ERC721.sol";
+
+contract FifaToken is ERC721 {
+	string _name;
+	string _symbol;
+
+	constructor () ERC721("FifaToken","FT") public {
+
+	}
+}
