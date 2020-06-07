0d3a15a2d5b746483889826ea4508a070045d1d7
Malhar Dave <malharvdave@gmail.com>
2020-06-06 22:25:44 -0400

    More mint test changes

--- test/FifaToken.test.js
+++ test/FifaToken.test.js
@@ -41,11 +41,20 @@ describe('minting', async() => {
 			country: "Argentina"
 		});
 		const totalSupply = await contract.totalSupply();
+		//SUCCESS
 		assert.equal(totalSupply,1);
 		const event = result.logs[0].args;
 		assert.equal(event.tokenId.toNumber(),0,'id is correct');
 		assert.equal(event.from,'0x0000000000000000000000000000000000000000','from is correct');
 		assert.equal(event.to,accounts[0],'to is correct');
+
+		//FAILURE
+		await contract.mint({
+			name: "Messi",
+			position: "Striker",
+			club: "FC Barcelona",
+			country: "Argentina"
+		}).should.be.rejected;
 	})
 })
 
